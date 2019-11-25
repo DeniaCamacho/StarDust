af0739a3ac806eff2dc67cd73bf6a85591f7652c
Denia Camacho <deniacamacho04@gmail.com>
2019-11-24 23:34:01 -0800

    design details

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -14,37 +14,39 @@ export default props => {
   }
 
   return (
-    <div className="chatMain">
-      <h1 className="hi"> Hello {username}!</h1>
+    <>
       <button id="signOut" onClick={e => signout()}>
         Sign Out
       </button>
+      <div className="chatMain">
+        <h1 className="hi"> Hello {username}!</h1>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          id="text"
-          type="text"
-          value={message}
-          onChange={e => setMessage(e.target.value)}
-        />
-        <button id="sub" type="submit">
-          Submit
-        </button>
-      </form>
-      <div id="chat">
-        <div id="users">
-          {users.map((u, i) => (
-            <p>{u.username}</p>
-          ))}
-        </div>
-        <div id="messages">
-          {messages.map((msg, i) => (
-            <p className="msg" key={"message" + i}>
-              {msg.username}:{msg.message}
-            </p>
-          ))}
+        <form onSubmit={handleSubmit}>
+          <input
+            id="text"
+            type="text"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+          <button id="sub" type="submit">
+            Send
+          </button>
+        </form>
+        <div id="chat">
+          <div id="users">
+            {users.map((u, i) => (
+              <p>{u.username}</p>
+            ))}
+          </div>
+          <div id="messages">
+            {messages.map((msg, i) => (
+              <p className="msg" key={"message" + i}>
+                {msg.username}:{msg.message}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
